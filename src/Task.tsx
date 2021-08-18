29e28362308c88031b2bb5adcaf5a8123511d07c
Davejs136 <davejs136@gmail.com>
2021-08-18 10:29:26 -0400

    add: components in task main component

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,35 +1,27 @@
-import React from 'react';
-
-type Task = {
-  id: string;
-  title: string;
-  description: string;
-  done: boolean;
-};
-
-const tasks: Task[] = [
-  {
-    id: 'aaa-bbb-ccc',
-    title: 'Make the design from figma template',
-    description: 'Is nedded to have a desing of new site',
-    done: false,
-  },
-];
+import React, { useState } from 'react';
+import TaskList from './components/TaskList';
+import useStore from './store';
 
 export default function Task() {
-  const handleChange = () => {};
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+  const { addTask, tasks } = useStore()
+
+  const reset = () => {
+    setTitle('')
+    setDescription('')
+  }
 
   return (
-    <ul>
-      {tasks.map((task) => (
-        <li key={task.id}>
-          <h3>{task.title}</h3>
-          <p>{task.description}</p>
-          <div>
-            <input type="checkbox" onChange={handleChange} />
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h2>TodoIt - Tasks</h2>
+      <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <textarea cols={30} rows={10} value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+      <button onClick={() => {
+        addTask(title, description)
+        reset()
+      }}>Add task</button>
+      <TaskList tasks={tasks} />
+    </div>
   );
 }
