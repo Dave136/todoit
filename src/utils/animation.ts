757703221b5bc3ec0dc3e4c1bd358917b1d126b8
Davejs136 <davejs136@gmail.com>
2022-02-13 16:10:19 -0400

    add: basic gsap animations

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,1 +1,18 @@
-animation.ts
+import gsap from 'gsap';
+
+export const fadeIn = (el: any) => {
+  gsap.from(el, {
+    opacity: 0,
+    duration: 0.7,
+    ease: 'power4.out',
+  });
+};
+
+export const fadeOut = (el: any, callback: VoidFunction) => {
+  gsap.to(el, {
+    opacity: 0,
+    duration: 0.5,
+    ease: 'power4.out',
+    onComplete: () => callback(),
+  });
+};
